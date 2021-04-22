bc74878244ee1b73689c634833c437d950e2a712
Andre Soares <andreiarley@gmail.com>
2021-04-22 04:43:28 -0400

    added add class form

--- components/editor/AulasForm.js
+++ components/editor/AulasForm.js
@@ -1,11 +1,35 @@
-import TextField from '@material-ui/core/TextField';
+import 'date-fns'
+import TextField from '@material-ui/core/TextField'
 import { useState } from 'react'
+import { makeStyles } from '@material-ui/core/styles'
 import Button from '@material-ui/core/Button'
+import FormControlLabel from '@material-ui/core/FormControlLabel'
+import Switch from '@material-ui/core/Switch'
+import DateFnsUtils from '@date-io/date-fns'
+import { 
+  MuiPickersUtilsProvider,
+  KeyboardDatePicker, 
+} from '@material-ui/pickers';
+
+const useStyles = makeStyles((theme) => ({
+    root: {},
+    formItem: {
+        width: '90%',
+        margin: '5px 5% 5px 5%'
+    }
+}))
+
+// var options = { year: 'numeric', month: 'long', day: 'numeric' }
+// .toLocaleDateString('pt-BR', options)
 
 export default function AulasForm(props) {
 
-    var options = { year: 'numeric', month: 'long', day: 'numeric' }
-    const newDate = new Date().toLocaleDateString('pt-BR', options)
+    const classes = useStyles()
+
+    const [ checked, setChecked ] = useState(true) 
+    const [ valid, setValid ] = useState(false) 
+    
+    const newDate = new Date()
 
     const [newAula, setNewAula] = useState({
         title: '',
@@ -14,10 +38,16 @@ export default function AulasForm(props) {
         date: newDate
     })
 
+    const handleDateChange = (date) => {
+        const params = {target: {name: 'date', value: date}}
+        handleNewAula(params)
+    };
+
 
     const handleNewAula = (event) => {
         const { value, name }  = event.target
-        if(name === 'drive' && value.length > 63){
+        
+        if(name === 'drive' && value.length > 65) {
 
             const id = value.slice(32, 65)
             const file = `https://drive.google.com/uc?export=download&id=` + id
@@ -27,20 +57,69 @@ export default function AulasForm(props) {
                 [name]: value,
                 ['file']: file
             }))
+
+            setValid(true)
+
         } else {
             setNewAula((preValue) => ({
                 ...preValue,
                 [name]: value
             }))
+
+            if (newAula.drive.length < 65) setValid(false)
+
         }
+        
+        
+    }
+    
+    const prepareAndSend = () => {
+        if(checked) {
+            setNewAula((preValue) => ({
+                ...preValue,
+                ['date']: newDate
+            }))
+        }
+        
+        if(valid) props.send(newAula)   
+             
     }
 
     return (
         <>
             <form>
-                <TextField onChange={handleNewAula} id="outlined-basic" label="Título da Aula" name="title" value={newAula.title} ariant="outlined" />
-                <TextField onChange={handleNewAula} id="outlined-basic" label="Link do arquivo no Drive" name="drive" value={newAula.drive} ariant="outlined" />
-                <Button onClick={() => props.send(newAula)}>Enviar</Button>
+                {!valid && (<p className={classes.formItem}>Insira um link do drive válido</p>)}
+                <TextField className={classes.formItem} onChange={handleNewAula} id="outlined-basic" label="Título da Aula" name="title" value={newAula.title} ariant="outlined" />
+                <TextField className={classes.formItem} onChange={handleNewAula} id="outlined-basic" label="Link do arquivo no Drive" name="drive" value={newAula.drive} ariant="outlined" />
+                <FormControlLabel
+                     className={classes.formItem}
+                    control={
+                    <Switch
+                        checked={checked}
+                        onChange={() => setChecked(!checked)}
+                        name="checkedB"
+                        color="primary"
+                    />
+                    }
+                    label="Data Atual"
+                />
+                {!checked && (
+                    <MuiPickersUtilsProvider utils={DateFnsUtils} >
+                        <KeyboardDatePicker
+                            className={classes.formItem}
+                            margin="normal"
+                            id="date-picker-dialog"
+                            label="Date picker dialog" 
+                            format="MM/dd/yyyy"
+                            value={newAula.date}
+                            onChange={handleDateChange}
+                            KeyboardButtonProps={{
+                                'aria-label': 'change date',
+                            }}
+                        />
+                    </MuiPickersUtilsProvider>
+                )}
+                <Button className={classes.formItem} onClick={prepareAndSend}>Enviar</Button>
             </form>
         </>
     )
--- components/index/AppBar.js
+++ components/index/AppBar.js
@@ -1,8 +1,9 @@
-import React from 'react';
-import { makeStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
-import Toolbar from '@material-ui/core/Toolbar';
-import Typography from '@material-ui/core/Typography';
+import React from 'react'
+import Link from 'next/link'
+import { makeStyles } from '@material-ui/core/styles'
+import AppBar from '@material-ui/core/AppBar'
+import Toolbar from '@material-ui/core/Toolbar'
+import Typography from '@material-ui/core/Typography'
 import Button from '@material-ui/core/Button';
 // import IconButton from '@material-ui/core/IconButton';
 // import MenuIcon from '@material-ui/icons/Menu';
@@ -35,7 +36,7 @@ export default function ButtonAppBar() {
           <Typography variant="h6" className={classes.title}>
             Sala de Aula
           </Typography>
-          <Button color="inherit">Login</Button>
+          <Link href={'/editor'}><Button color="inherit">Login</Button></Link>
         </Toolbar>
       </AppBar>
     </div>
